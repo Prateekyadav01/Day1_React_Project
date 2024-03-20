ff8cff217d915a181738a401f8d6cb7c55bfca69
PrateekTechmatys <prateeky562@gmail.com>
2024-03-20 16:21:11 +0530

    1st part is done

--- src/components/Notes/HomeNotes.jsx
+++ src/components/Notes/HomeNotes.jsx
@@ -9,7 +9,7 @@ const HomeNotes = ({ homeNotes }) => {
 
   useEffect(() => {
     handleTheLocalStorage();
-  }, [])
+  }, [homeNotes])
 
   const [answer, setAnswer] = useState([]);
   const handleTheLocalStorage = () => {
@@ -24,7 +24,7 @@ const HomeNotes = ({ homeNotes }) => {
       <div className='container mx-auto'>
         <h1 className="text-3xl font-bold mb-8 text-center">Home Notes</h1>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {answer && answer.map((item, index) => (
+          {answer.map((item, index) => (
             <Card item={item} key={index} />
           ))}
         </div>
