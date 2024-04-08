cc460e6fc85f1a799e9b2bf84c272bcb5c930883
PrateekTechmatys <prateeky562@gmail.com>
2024-04-08 11:21:12 +0530

    react_Project

--- src/components/Notes/Card.jsx
+++ src/components/Notes/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ item }) => {
   return (
     <div className='flex flex-wrap mx-2 my-2 border border-gray-300 rounded p-4 overflow-hidden bg-white text-gray-800 text-base font-medium'>
-      <p className="text-sm overflow-y-auto max-h-full break-all">{item}</p>
+      <p className="text-sm overflow-y-auto max-h-full break-all">Item is this {item}</p>
     </div>
   );
 };
--- src/components/Notes/HomeNotes.jsx
+++ src/components/Notes/HomeNotes.jsx
@@ -1,32 +1,18 @@
 import React, { useEffect, useState } from 'react';
-
-import Card from './Card';
+import Card from "./Card"
 
 const HomeNotes = ({ homeNotes }) => {
 
-
-  console.log(homeNotes);
-
-  useEffect(() => {
-    handleTheLocalStorage();
-  }, [homeNotes])
-
-  const [answer, setAnswer] = useState([]);
-  const handleTheLocalStorage = () => {
-    let temp = localStorage.getItem('homeNotes') ? JSON.parse(localStorage.getItem('homeNotes')) : [];
-    setAnswer(temp);
-    console.log(answer)
-  };
-
+  const [notes , setNodes] = useState([]);
 
   return (
-    <div className='bg-gray-500 py-8 min-h-[100vh]' >
+    <div className='bg-gray-500 py-8 min-h-[100vh]'>
       <div className='container mx-auto'>
         <h1 className="text-3xl font-bold mb-8 text-center">Home Notes</h1>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {answer.map((item, index) => (
-            <Card item={item} key={index} />
-          ))}
+
+
+          <p>{notes}</p>
         </div>
       </div>
     </div>
